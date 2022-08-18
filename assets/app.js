b00ccc5b11cfadef39aff3a3a8cb6357bc61f4a3
mehwish <mehwishgul715@gmail.com>
2022-08-18 21:09:09 +0500

    commit

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,24 @@
-let createAccount=document.getElementById("create-account");
-let fullName=document.getElementById("full-name");
-let yourName=document.getElementById("your-name");
-let Name="";
-createAccount.addEventListener("click",function (){
-     Name = fullName.value ;
-})
-yourName.value = Name;
+$(document).ready(function(){
+    let selector=$('.all').attr('data-filter');
+    $('.portfolio .grid').isotope({
+      filter:selector,
+  
+    });
+    return false;
+  
+  });
+  $(document).ready(function(){
+  let btn=$('.state__list');
+  
+    btn.click(function(e){
+      $('.state__list button').removeClass("active-port");
+      e.target.classList.add("active-port");
+  
+      let selector=$(e.target).attr('data-filter');
+      $('.portfolio .grid').isotope({
+        filter:selector
+      });
+      return false;
+    });
+    
+  });
